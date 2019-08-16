6fe7bda94cffe1b4b013001f7fa657a7367545e6
Oluwalayomi Akinrinade <layomia@gmail.com>
2019-08-16 18:54:18 -0400

    Add scenarios and answers

---
+++ Scenario4/Scenario4/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Scenario4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song = new Song()
+            {
+                Title = "Hey Jude",
+                Artist = "The Beatles",
+                Genre = Genre.PopRock,
+            };
+
+            string json = Serialize(song);
+            Console.WriteLine(json);
+            
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadLine();
+        }
+
+        // TODO 1) Serialize the "Song" object to a JSON representation where the Genre property is written as a string.
+        // For example, {"Title":"Three Little Birds","Artist":"Bob Marley","Genre":"Reggae"}
+        private static string Serialize(Song song)
+        {
+            // <Add/modify code here>
+            return "";
+        }
+    }
+
+    public enum Genre { Classical, Rap, Soul, PopRock, Reggae };
+
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
