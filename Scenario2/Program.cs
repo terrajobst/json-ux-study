b65e0c7fef8f79f51bb2aa6a41016d3062ccd63e
Katarzyna Bulat <t-kabul@microsoft.com>
2019-09-10 20:36:17 -0700

    spacing fix

--- Scenario2/Program.cs
+++ Scenario2/Program.cs
@@ -16,7 +16,7 @@ namespace Scenario2
         /* TODO: Return a string containing properties from a given config file with following modifications:
          1) Multiply all values by 2.
          2) Insert a property "input_frequency" with value 6.
-         3)	Make sure the resulting JSON only contains numeric values without dropping any properties. */
+         3) Make sure the resulting JSON only contains numeric values without dropping any properties. */
         private static string DoubleAllProperties(string configuration)
         {
             throw new NotImplementedException();
