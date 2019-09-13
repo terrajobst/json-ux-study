741ea255e4b904d3bbe5a864d8f27339c9e576c3
Immo Landwerth <immol@microsoft.com>
2019-09-12 18:03:49 -0700

    Change wording of scenarios and other improvements

--- Scenario1/Program.cs
+++ Scenario1/Program.cs
@@ -9,25 +9,29 @@ namespace Scenario1
         {
             string employee = GetEmployee();
             Console.WriteLine(employee);
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
         }
 
         /* TODO: 
-         1) Create an in-memory Document Object Model (DOM) that represents the following JSON:
+         1) Programmatically create the JSON tree structure, also known as the
+            Document Object Model (DOM), for the following JSON text.
+            DO NOT use parsing or serialization.
+
             {
                 "name": "John Smith",
                 "is manager": true, 
-                "reporting FTE employees ids": [ 1234, 54678, 90, 123, 56789 ],
-                "reporting intern employees ids" : [],
+                "employee ids": [ 1234, 54678, 90 ],
                 "direct supervisor" : null,
                 "address": 
                 {
                     "address line 1": "One Microsoft Way",
-                    "city": "Redmond",
-                    "zip code": 98052,
-                    "state": "WA"  
+                    "zip code": 98052
                 }
             }
-         2) Return it as a string. */
+
+         2) Convert the DOM representation to its JSON text and return that. */
         private static string GetEmployee()
         {
             // TODO: Add/modify code here
--- Scenario3/Program.cs
+++ Scenario3/Program.cs
@@ -10,13 +10,16 @@ namespace Scenario3
             JsonElement student = GetStudent();
             student = ModifyStudent(student);
             Console.WriteLine(student.ToString());
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
         }
 
         /* TODO: 
          1) Modify the given JsonElement by:
             * changing the e-mail property to "annp@mail.com"
             * adding a grade of 55 between 60 and 85 in the grades array.
-         2) Return back the modified JSON as an instance of JsonElement.*/
+         2) Return back the modified JSON as an instance of JsonElement. */
         private static JsonElement ModifyStudent(JsonElement student)
         {
             // TODO: Add/modify code here
@@ -30,13 +33,14 @@ namespace Scenario3
         private static JsonElement GetStudent()
         {
             string jsonString = @"
-            {
-                ""first name"" : ""Ann"",
-                ""last name"" : ""Predictable"",
-                ""email"" : ""ann.predictable@mail.com"",
-                ""grades"": [70, 60, 85]
-            }";
-            
+{
+    ""first name"" : ""Ann"",
+    ""last name"" : ""Predictable"",
+    ""email"" : ""ann.predictable@mail.com"",
+    ""grades"": [70, 60, 85]
+}
+            ";
+
             JsonDocument document = JsonDocument.Parse(jsonString);
             return document.RootElement;
         }
